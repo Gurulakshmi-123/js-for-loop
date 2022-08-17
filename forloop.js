c3d94256fffbc801569ab39af0fb946745b02ead
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-17 17:48:19 +0530

    for loop concept

---
+++ forloop.js
@@ -0,0 +1,16 @@
+// program to display text 5 times
+var n = 5;
+
+// looping from i = 1 to 5
+for (var i = 1; i <= n; i++) 
+{
+    console.log('javascript is a scripting language');
+}
+
+
+
+
+for(var i=1; i<=5; i++)
+{
+    console.log(i + "<br/>")
+}
